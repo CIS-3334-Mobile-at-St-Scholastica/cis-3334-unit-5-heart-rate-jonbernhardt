25fd71c99c883278275b5e2647c2217904733987
Jon Bernhardt <bernhardt.jonathan@mayo.edu>
2018-02-13 10:21:37 -0600

    Added heart rate adapter

---
+++ app/src/main/java/css/cis3334/heartratetracker/HeartRateAdapter.java
@@ -0,0 +1,58 @@
+package css.cis3334.heartratetracker;
+
+        import android.app.Activity;
+        import android.content.Context;
+        import android.support.v4.content.ContextCompat;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
+        import android.widget.ArrayAdapter;
+        import android.widget.TextView;
+
+/**
+ * Created by Tom Gibbons in Feb 2017.
+ * For the CIS 3334 class at St. Scholastica
+ */
+
+public class HeartRateAdapter  extends ArrayAdapter<HeartRate> {
+
+    private final Context context;      // The activity calling this adapter
+    private HeartRateList hrList;       // The object holding the arraylist of hear rates
+
+    /**
+     *
+     * @param context The activity calling this adapter
+     * @param rowLayout The xml file defining the layout for one item or row in the list
+     * @param dummyTV the ID for a TextView in the row layout. Not used, but needed by the parent object
+     * @param hrList The object holding the arraylist of hear rates
+     */
+    public HeartRateAdapter(Context context, int rowLayout, int dummyTV, HeartRateList hrList) {
+        super(context, rowLayout, dummyTV, hrList.getList());
+        this.context = context;
+        this.hrList = hrList;
+    }
+
+    /**
+     * This is called automatically to display each item in the list.
+     *    Here you must fill the layout for one row or item in the list
+     *
+     * @param position index in the list that is being selected
+     * @param convertView
+     * @param parent The parent layout this list is in
+     * @return
+     */
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        //
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.heart_rate_row, null);
+        //get the heart rate we are displaying
+        HeartRate hr = hrList.getHeartRate(position);
+
+        TextView tvPulse=(TextView)view.findViewById(R.id.textViewPulse);
+        tvPulse.setText(hr.getPulse().toString());
+
+        return(view);
+    }
+
+}
--- app/src/main/java/css/cis3334/heartratetracker/MainActivity.java
+++ app/src/main/java/css/cis3334/heartratetracker/MainActivity.java
@@ -2,17 +2,41 @@ package css.cis3334.heartratetracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     HeartRateList heartRateList;        // The list of heart rate objects
+    ArrayAdapter<HeartRate> hrAdapter;  // The custom array adapter for displaying the heart rates in the list view
+    ListView lvHeartRates;              // The list view for the heart rates from the activity_mail.xml file
+    TextView tvSelect;                  // The text view for the selected rate the user clicks on from the activity_mail.xml file
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        lvHeartRates = (ListView) findViewById(R.id.lvHeartRateList);
+        tvSelect = (TextView) findViewById(R.id.tvSelectText);
+
         heartRateList = new HeartRateList();
         heartRateList.InitRandomElderly();
+
+        hrAdapter = new HeartRateAdapter(this, R.layout.heart_rate_row, R.id.textViewPulse, heartRateList);
+        hrAdapter.setDropDownViewResource(R.layout.heart_rate_row);
+        lvHeartRates.setAdapter(hrAdapter);
+
+        lvHeartRates.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, final View view, int position, long id) {
+                HeartRate hr = (HeartRate) parent.getItemAtPosition(position);
+                tvSelect.setText("You selected: " + hr.toString());
+            }
+        });
     }
 }
